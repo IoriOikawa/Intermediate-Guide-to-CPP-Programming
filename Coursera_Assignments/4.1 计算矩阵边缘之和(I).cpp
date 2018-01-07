24b008a60a5010c47cfbfcfcd1d09c8e5ead4bd3
IoriOikawa <iorioikawa@yahoo.co.jp>
2018-01-07 13:46:55 +0800

    Try not to touch the knowledge of pointers...

---
+++ Coursera_Assignments/4.1 计算矩阵边缘之和(I).cpp
@@ -0,0 +1,39 @@
+// 虽然课程已经进行到需要动用指针知识的时候了，
+// 但是某傻喵还是向偷个懒再对付一把。。
+// 于是。。。
+
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	
+	int k, l, t;
+	
+	cin >> k;
+	
+	while (k > 0) {
+		
+		int m, n, i, j;
+		int sum = 0;
+		
+		cin >> m >> n;
+		
+		for (i = 0; i < m; i++) 
+		{
+			for (j = 0; j < n; j++) 
+			{
+				cin >> t;
+				
+				if (i == 0 || i == m-1 || j == 0 || j == n - 1) {
+					sum += t;
+				}
+			}
+		}
+		
+		cout << sum << endl;
+		k--;
+	}
+	return 0;
+}
